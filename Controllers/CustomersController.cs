936a02894a1a77fc6489512ca189ce59a6a8c4f5
Bishop Bader <bishop.bader@gmail.com>
2020-07-16 16:49:46 -0400

    Updated Customers controller and views

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: customers/
+        [Route("customers/")]
+        public ActionResult Customers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+
+            var viewModel = new CustomerViewModel
+            {
+                Customer = customers
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: customers/details/id
+        [Route("customers/details/{id}")]
+        public ActionResult Details()
+        {
+            
+            return View();
+        }
+    }
+}
