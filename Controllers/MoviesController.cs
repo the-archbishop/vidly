fd0dfee1d960352d0e8be4beea2ce6c97b77d3de
Bishop Bader <bishop.bader@gmail.com>
2020-07-16 11:09:50 -0400

    Added/updated Movies and Customers, broke out navbar to partial

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -9,11 +9,19 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        // GET: Movies/
+        [Route("movies/")]
+        public ActionResult Movies()
         {
             var movie = new Movie() { Name = "Shrek" };
             return View(movie);
         }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
     }
 }
